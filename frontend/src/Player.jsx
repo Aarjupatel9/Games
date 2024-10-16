48cccf82edb82c849f3440acbc62d7c6d8117705
Aarju patel <aarjupatel922003@gmail.com>
2024-10-16 22:52:28 +0530

    fix issue in player move, player wil force to move if touch by user

--- frontend/src/Player.jsx
+++ frontend/src/Player.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { UserIcon } from './assets/svgs';
 
+
 export default function Player(props) {
-    const { name, isActive, color } = props.player;
+    const { name, isActive, color, } = props.user1;
+    const possition = props.possition;
+    useEffect(() => {
+        console.log("____", Array.from({ length: props.user2.retirePlayer }, (_, index) => index), props.user2.retirePlayer, possition)
+    }, [])
     return (
-        <div className='Player content-center justify-center flex flex-col'>
-            <div className={`rounded shadow-cyan-500/50 ${isActive ? "shadow-xl animate-bounce" : "shadow-sm"}`}>
-                <div className={color}>
-                    <UserIcon color={color} />
+        <div className={`Player flex ${possition == "right" ? "flex-row-reverse" : "flex-row"} items-center justify-center`}>
+            <div className='flex flex-col content-center justify-center'>
+                <div className={`rounded shadow-cyan-500/50 ${isActive ? "shadow-xl animate-bounce" : "shadow-sm"}`}>
+                    <div className={color}>
+                        <UserIcon color={color} />
+                    </div>
                 </div>
+                {name}
             </div>
-            {name}
+            <div className={`flex content-center justify-center mx-4 flex-row flex-wrap gap-1`}>
+                {Array.from({ length: props.user2.retirePlayer }, (_, index) => index).map((_, index) => {
+                    return (<div
+                        key={`retirePlayer2-circle-${index}`}
+                        className={` rounded-full cursor-pointer ` + props.user2.color}
+                        style={{
+                            width: "18px",
+                            height: "18px",
+                            transform: "translate(-50%, -50%)"
+                        }}
+                    ></div>)
+                })}
+            </div>
         </div>
     )
 }
